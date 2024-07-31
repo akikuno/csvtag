8f8662a60f0f0d41ed5b76a14c480ad5a33b7560
akikuno <akikuno@users.noreply.github.com>
2024-07-31 10:32:23 +0900

    Add call_csvtag

--- src/csvtag/caller.py
+++ src/csvtag/caller.py
@@ -4,27 +4,49 @@ from collections.abc import Iterator
 from itertools import groupby
 from pathlib import Path
 
+import cstag
+
 from csvtag.microhomology_handler import remove_microhomology
-from csvtag.sam_handler import extract_alignment, read_sam, remove_overlapped_alignments
-
-# 入力: samのalignment: Iterator[str]
-# 出力: {QNAME: csvtag}: Iterator[dict[str, str]]
-# Inversionはリファレンスに合わせる（＝revcompする）
+from csvtag.sam_handler import extract_alignment, is_forward_strand, read_sam, remove_overlapped_alignments
 
 
-def call_csvtag(path_sam: str | Path) -> dict[str, str]:
+def convert_to_csvtag(alignments: list[dict[str, str | int]]) -> str:
+    idx = 0
+    while idx < len(alignments) - 1:
+        curr_align = alignments[idx]
+        next_align = alignments[idx + 1]
+
+        curr_flag: int = curr_align["FLAG"]
+        next_flag: int = next_align["FLAG"]
+        curr_cstag: str = curr_align["CSTAG"]
+        next_cstag: str = next_align["CSTAG"]
+
+        if is_forward_strand(curr_flag) != is_forward_strand(next_flag):
+            if is_forward_strand(curr_flag):
+                curr_align["CSTAG"] = curr_cstag.upper()
+                next_align["CSTAG"] = cstag.revcomp(next_cstag).lower()
+            else:
+                curr_align["CSTAG"] = cstag.revcomp(curr_cstag).lower()
+                next_align["CSTAG"] = next_cstag.upper()
+
+            next_align["FLAG"] -= 16
+        idx += 1
+
+    return "".join(align["CSTAG"] for align in alignments)
+
+
+def call_csvtag(path_sam: str | Path) -> Iterator[dict[str, str]]:
     alignments: Iterator[dict[str, str | int]] = extract_alignment(read_sam(path_sam))
-    # バグを未然に防ぐため、マイクロホモロジー以外の重複を除く
     alignments = remove_overlapped_alignments(alignments)
-    # cstagにあるマイクロホモロジーをトリムする
     alignments = remove_microhomology(alignments)
-    # 逆位を検出し、revcompする
+
     alignments = list(alignments)
     alignments.sort(key=lambda x: (x["QNAME"], x["POS"]))
-    for _, alignments_grouped in groupby(alignments, key=lambda x: x["QNAME"]):
+    for qname, alignments_grouped in groupby(alignments, key=lambda x: x["QNAME"]):
         alignments_grouped = list(alignments_grouped)
         if len(alignments_grouped) == 1:
-            yield from alignments_grouped
+            for alignment in alignments_grouped:
+                yield {qname: alignment["CSTAG"]}
             continue
-        # 逆位の検出：
-    # {QNAME: CSVTAG}の辞書を作成する
+        csv_tag = convert_to_csvtag(alignments_grouped)
+        yield {qname: csv_tag}
--- src/csvtag/inversion_handler.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-import cstag
-import midsv
-from csvtag.sam_handler import is_forward_strand
-
-
