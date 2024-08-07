65515436e454929d50242dfb3651b44ead9802e5
akikuno <akikuno@users.noreply.github.com>
2024-08-07 12:54:00 +0900

    Add `trim_microhomology`

--- src/csvtag/microhomology_handler.py
+++ src/csvtag/microhomology_handler.py
@@ -3,25 +3,33 @@ from __future__ import annotations
 from collections.abc import Iterator
 from itertools import groupby
 
-import cstag
-
 from csvtag.combiner import combine_splitted_tags
-from csvtag.sam_handler import trim_softclip
 from csvtag.splitter import split_by_nucleotide as split
+from csvtag.to_sequence import to_sequence
 
 ###########################################################
 # remove_microholomogy
 ###########################################################
 
 
-def _get_length_of_microhomology(curr_sequence: str, next_sequence: str, curr_qual: str, next_qual: str) -> int:
+def _get_length_of_microhomology(curr_sequence: str, next_sequence: str) -> int:
     len_microhomology = 0
     min_length = min(len(curr_sequence), len(next_sequence))
 
     for i in range(1, min_length + 1):
-        if curr_sequence[-i:] == next_sequence[:i] and curr_qual[-i:] == next_qual[:i]:
+        if curr_sequence[-i:] == next_sequence[:i]:  # and curr_qual[-i:] == next_qual[:i]:
             len_microhomology = i
     return len_microhomology
+
+
+# def _get_length_of_microhomology(curr_sequence: str, next_sequence: str, curr_qual: str, next_qual: str) -> int:
+#     len_microhomology = 0
+#     min_length = min(len(curr_sequence), len(next_sequence))
+
+#     for i in range(1, min_length + 1):
+#         if curr_sequence[-i:] == next_sequence[:i] and curr_qual[-i:] == next_qual[:i]:
+#             len_microhomology = i
+#     return len_microhomology
 
 
 def _trim_microhomology(
@@ -38,20 +46,20 @@ def _trim_microhomology(
         curr_cstag: str = curr_align["CSTAG"]
         next_cstag: str = next_align["CSTAG"]
 
-        curr_cigar: str = curr_align["CIGAR"]
-        next_cigar: str = next_align["CIGAR"]
-        curr_qual: str = curr_align["QUAL"]
-        next_qual: str = next_align["QUAL"]
+        # curr_cigar: str = curr_align["CIGAR"]
+        # next_cigar: str = next_align["CIGAR"]
+        # curr_qual: str = curr_align["QUAL"]
+        # next_qual: str = next_align["QUAL"]
 
-        curr_qual = trim_softclip(curr_qual, curr_cigar)
-        next_qual = trim_softclip(next_qual, next_cigar)
+        # curr_qual = trim_softclip(curr_qual, curr_cigar)
+        # next_qual = trim_softclip(next_qual, next_cigar)
 
         # Check the length of microhomology
         len_microhomology = _get_length_of_microhomology(
-            cstag.to_sequence(curr_cstag),
-            cstag.to_sequence(next_cstag),
-            curr_qual,
-            next_qual,
+            to_sequence(curr_cstag),
+            to_sequence(next_cstag),
+            # curr_qual,
+            # next_qual,
         )
         # Continue if no microhomology exists
         if len_microhomology == 0:
@@ -78,11 +86,46 @@ def remove_microhomology(
     alignments: Iterator[dict[str, str | int]],
 ) -> Iterator[dict[str, str | int]]:
     alignments = list(alignments)
-    alignments.sort(key=lambda x: (x["QNAME"], x["POS"]))
+    alignments.sort(key=lambda x: (x["QNAME"], x["RNAME"], x["POS"]))
 
-    for _, alignments_grouped in groupby(alignments, key=lambda x: x["QNAME"]):
+    for _, alignments_grouped in groupby(alignments, key=lambda x: [x["QNAME"], x["RNAME"]]):
         alignments_grouped = list(alignments_grouped)
         if len(alignments_grouped) == 1:
             yield from alignments_grouped
             continue
         yield from _trim_microhomology(alignments_grouped)
+
+
+###########################################################
+# Handle csv tag
+###########################################################
+
+
+def trim_microhomology(csv_tags: list[str]) -> list[str]:
+    """
+    csv_tags = ["=AAATTT", "=TTTCCC"]
+    trim_microhomology(csv_tags)
+    ["=AAATTT", "=CCC"]
+    """
+    csv_tags_trimmed = []
+    visited = set()
+    for i in range(len(csv_tags) - 1):
+        curr_csvtag, next_csvtag = csv_tags[i], csv_tags[i + 1]
+
+        # Check the length of microhomology
+        len_microhomology = _get_length_of_microhomology(
+            to_sequence(curr_csvtag.upper()), to_sequence(next_csvtag.upper())
+        )
+
+        csvtag_trimmed = list(split(next_csvtag))[len_microhomology:]
+        next_csvtag = combine_splitted_tags(csvtag_trimmed)
+
+        if i not in visited:
+            csv_tags_trimmed.append(curr_csvtag)
+
+        csv_tags_trimmed.append(next_csvtag)
+
+        visited.add(i)
+        visited.add(i + 1)
+
+    return csv_tags_trimmed
--- tests/src/test_microhomology_handler.py
+++ tests/src/test_microhomology_handler.py
@@ -2,23 +2,37 @@ from __future__ import annotations
 
 import pytest
 
-from csvtag.microhomology_handler import _get_length_of_microhomology, _trim_microhomology
+from csvtag.microhomology_handler import _get_length_of_microhomology, _trim_microhomology, trim_microhomology
+
+# @pytest.mark.parametrize(
+#     "curr_sequence, next_sequence, curr_qual, next_qual, expected",
+#     [
+#         ("AAAA", "GGGG", "!!!!", "!!!!", 0),  # No overlap
+#         ("AAGG", "GGCC", "!!!!", "!!!!", 2),  # 2 bases overlap
+#         ("AGCT", "AGCT", "!!!!", "!!!!", 4),  # "AGCT" overlap
+#         ("TAGCT", "AGCT", "!!!!!", "!!!!", 4),  # "AGCT" overlap
+#         ("TAGCT", "AGCTA", "!!!!!", "!!!!!", 4),  # "AGCT" overlap
+#         ("AAAA", "AAAA", "!!!!", "@@@@", 0),  # No overlap due to mismatch at quality
+#         ("AAAA", "AAAA", "!!!!", "!!@@", 2),  # "AA" overlap
+#     ],
+# )
+# def test_get_length_of_microhomology(curr_sequence, next_sequence, curr_qual, next_qual, expected):
+#     assert _get_length_of_microhomology(curr_sequence, next_sequence, curr_qual, next_qual) == expected
 
 
 @pytest.mark.parametrize(
-    "curr_sequence, next_sequence, curr_qual, next_qual, expected",
+    "curr_sequence, next_sequence, expected",
     [
-        ("AAAA", "GGGG", "!!!!", "!!!!", 0),  # No overlap
-        ("AAGG", "GGCC", "!!!!", "!!!!", 2),  # 2 bases overlap
-        ("AGCT", "AGCT", "!!!!", "!!!!", 4),  # "AGCT" overlap
-        ("TAGCT", "AGCT", "!!!!!", "!!!!", 4),  # "AGCT" overlap
-        ("TAGCT", "AGCTA", "!!!!!", "!!!!!", 4),  # "AGCT" overlap
-        ("AAAA", "AAAA", "!!!!", "@@@@", 0),  # No overlap due to mismatch at quality
-        ("AAAA", "AAAA", "!!!!", "!!@@", 2),  # "AA" overlap
+        ("AAAA", "GGGG", 0),  # No overlap
+        ("AAGG", "GGCC", 2),  # 2 bases overlap
+        ("AGCT", "AGCT", 4),  # "AGCT" overlap
+        ("TAGCT", "AGCT", 4),  # "AGCT" overlap
+        ("TAGCT", "AGCTA", 4),  # "AGCT" overlap
+        ("AAAA", "AAAA", 4),  # No overlap due to mismatch at quality
     ],
 )
-def test_get_length_of_microhomology(curr_sequence, next_sequence, curr_qual, next_qual, expected):
-    assert _get_length_of_microhomology(curr_sequence, next_sequence, curr_qual, next_qual) == expected
+def test_get_length_of_microhomology(curr_sequence, next_sequence, expected):
+    assert _get_length_of_microhomology(curr_sequence, next_sequence) == expected
 
 
 @pytest.mark.parametrize(
@@ -56,6 +70,26 @@ def test_get_length_of_microhomology(curr_sequence, next_sequence, curr_qual, ne
         ),
     ],
 )
-def test_trim_microhomology(alignments, expected):
+def test__trim_microhomology(alignments, expected):
     result = _trim_microhomology(alignments)
+    assert result == expected, f"Expected {expected}, but got {result}"
+
+
+###########################################################
+# Handle csv tag
+###########################################################
+
+
+@pytest.mark.parametrize(
+    "csv_tags, expected",
+    [
+        (["=AAATTT", "=TTTCCC"], ["=AAATTT", "=CCC"]),
+        (["=AAATTT", "=TTTCCC", "=CCGG"], ["=AAATTT", "=CCC", "=GG"]),
+        (["=AAATTT", "=tttccc"], ["=AAATTT", "=ccc"]),
+        (["=aaattt", "=TTTCCC"], ["=aaattt", "=CCC"]),
+        (["=A", "=C", "=G", "=T"], ["=A", "=C", "=G", "=T"]),
+    ],
+)
+def test_trim_microhomology(csv_tags, expected):
+    result = trim_microhomology(csv_tags)
     assert result == expected, f"Expected {expected}, but got {result}"
